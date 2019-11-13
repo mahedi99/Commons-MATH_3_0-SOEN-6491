e41b06aedcee97780a7457f0df5c5bff8805ab09
Arnav Bhardwaj <arnavrbhardwaj@gmail.com>
2019-11-13 04:04:45 -0500

    Update SparseRealVectorTest.java
    removing the properties that are already pulled up

--- src/test/java/org/apache/commons/math3/linear/SparseRealVectorTest.java
+++ src/test/java/org/apache/commons/math3/linear/SparseRealVectorTest.java
@@ -57,23 +57,15 @@ import org.apache.commons.math3.analysis.function.Power;
  *
  * @version $Id$
  */
-public class SparseRealVectorTest {
+public class SparseRealVectorTest extends IntermediateRealVectorTests {
 
-    //
+    // moved properties to IntermediateRealVectorTest class as it was type 1 clone
+	// removed ma1
     protected double[][] ma1 = {{1d, 2d, 3d}, {4d, 5d, 6d}, {7d, 8d, 9d}};
-    protected double[] vec1 = {1d, 2d, 3d};
-    protected double[] vec2 = {4d, 5d, 6d};
-    protected double[] vec3 = {7d, 8d, 9d};
-    protected double[] vec4 = {1d, 2d, 3d, 4d, 5d, 6d, 7d, 8d, 9d};
-    protected double[] vec5 = { -4d, 0d, 3d, 1d, -6d, 3d};
-    protected double[] vec_null = {0d, 0d, 0d};
-    protected Double[] dvec1 = {1d, 2d, 3d, 4d, 5d, 6d, 7d, 8d, 9d};
     protected double[][] mat1 = {{1d, 2d, 3d}, {4d, 5d, 6d},{ 7d, 8d, 9d}};
 
     // tolerances
     protected double entryTolerance = 10E-16;
-    protected double normTolerance = 10E-14;
-
     // Testclass to test the RealVector interface
     // only with enough content to support the test
     public static class SparseRealVectorTestImpl extends RealVector implements Serializable {
