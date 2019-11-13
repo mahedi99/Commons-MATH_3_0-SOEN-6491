670c6545346b7ccc7a7c91b49cd94d436e7dc971
Arnav Bhardwaj <arnavrbhardwaj@gmail.com>
2019-11-13 04:02:08 -0500

    Create IntermediateRealVectorTests.java
    abstract class that is needed to extract and pull up properties in common excluding the unused ones

---
+++ src/test/java/org/apache/commons/math3/linear/IntermediateRealVectorTests.java
@@ -0,0 +1,18 @@
+package org.apache.commons.math3.linear;
+
+public class IntermediateRealVectorTests {
+
+	protected double[] vec1 = {1d, 2d, 3d};
+	protected double[] vec2 = {4d, 5d, 6d};
+	protected double[] vec3 = {7d, 8d, 9d};
+	protected double[] vec4 = {1d, 2d, 3d, 4d, 5d, 6d, 7d, 8d, 9d};
+	protected double[] vec5 = { -4d, 0d, 3d, 1d, -6d, 3d};
+	protected double[] vec_null = {0d, 0d, 0d};
+	protected Double[] dvec1 = {1d, 2d, 3d, 4d, 5d, 6d, 7d, 8d, 9d};
+	protected double normTolerance = 10E-14;
+
+	public IntermediateRealVectorTests() {
+		super();
+	}
+
+}
